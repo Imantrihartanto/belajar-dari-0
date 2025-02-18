84edee4e9c6c882596eb9f65c2860e90b8d8cdf9
Imantrihartanto <imantrihartanto24@gmail.com>
2025-02-18 11:28:32 +0700

    upload pertama kali

---
+++ dasar1.js
@@ -0,0 +1,31 @@
+// belaajar fariabel java script
+var a = "a";
+let b = "b";
+const c ="c";
+
+//belajar fariabel text,angka,bolean
+let nama ="iman";
+let umur = 22;
+let apakahbenar = true ;
+let uang =500 ;
+
+//operasi arifmatika
+let angka1 =11;
+let angka2 =22;
+let angka3 =33;
+console.log =(angka1 + angka2 + angka3);
+console.log =(angka1 - angka2 - angka3);
+console.log =(angka1 * angka2 - angka3);
+console.log =(angka1 + angka2 / angka3);
+
+let penampunghasiloperatorarimatika = angka1 + angka2 + angka3 ;
+console.log = (penampunghasiloperatorarimatika);
+
+penampunghasiloperatorarimatika = angka1 - angka2 - angka3 ;
+console.log = (penampunghasiloperatorarimatika);
+
+penampunghasiloperatorarimatika = angka1 * angka2 - angka3 ;
+console.log = (penampunghasiloperatorarimatika);
+
+penampunghasiloperatorarimatika = angka1 + angka2 / angka3 ;
+console.log = (penampunghasiloperatorarimatika);
---
+++ dasar2.js
@@ -0,0 +1,52 @@
+// belaajar fariabel java script
+var a = "a";
+let b = "b";
+const c ="c";
+
+//belajar fariabel text,angka,bolean
+let nama ="iman";
+let umur = 22;
+let apakahbenar = true ;
+let uang =500 ;
+
+//operasi arifmatika
+let angka1 =11;
+let angka2 =22;
+let angka3 =33;
+console.log (angka1 + angka2 + angka3,'pertambahan');
+console.log (angka1 - angka2 - angka3,'pengurangan');
+console.log (angka1 * angka2 - angka3,'perkalian');
+console.log (angka1 + angka2 / angka3, 'pembagian');
+
+//belajar mengisi variable
+
+let penampunghasiloperatorarimatika = angka1 + angka2 + angka3 ;
+console.log(penampunghasiloperatorarimatika);
+
+penampunghasiloperatorarimatika = angka1 - angka2 - angka3 ;
+console.log(penampunghasiloperatorarimatika);
+
+penampunghasiloperatorarimatika = angka1 * angka2 - angka3 ;
+console.log(penampunghasiloperatorarimatika);
+
+penampunghasiloperatorarimatika = angka1 + angka2 / angka3 ;
+console.log(penampunghasiloperatorarimatika);
+
+//belajar past 2 nama
+let fullname = "iman trihartanto";
+let fristname = "iman";
+let lastname ="trihartanto"
+console.log(fullname);
+console.log(fristname + " " +lastname);
+console.log(fristname -lastname);//nan
+
+//logika angka
+let number = 11;
+console.log(number +10);//21
+number = 20;
+console.log(number + 10 );//30
+number = false;
+console.log(number + 10 );//10
+
+
+
